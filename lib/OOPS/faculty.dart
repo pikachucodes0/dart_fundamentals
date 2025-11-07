47f5418ad0331496fefd59e42b747c6006dfc55b
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-07 12:35:07 +0545

    Day4

--- lib/OOPS/faculty.dart
+++ lib/OOPS/faculty.dart
@@ -2,48 +2,84 @@ class Faculty{
   final String firstName;
   final String lastName;
   final String department;
-  final double salary;
+ 
 
   //Constructor
   Faculty({
     required this.firstName,
     required this.lastName,
-    required this.department,
-    required this.salary});
+    required this.department
+    });
 
   void displayFullName(){
     print("Name: $firstName $lastName");
   }
 
   double calculateAnnualSal(){
+    return 0;
+  }
+}
+
+class FullTimeFaculty extends Faculty{
+
+  final double salary;
+  FullTimeFaculty({
+    required super.firstName,
+    required super.lastName,
+    required super.department,
+    required this.salary,
+  });
+
+  @override
+  double calculateAnnualSal(){
     return 12*salary;
   }
 }
 
+class PartTimeFaculty extends Faculty{
+  final double hoursWorked;
+  final double hourlyRate;
+  PartTimeFaculty({
+    required super.firstName,
+    required super.lastName,
+    required super.department,
+    required this.hourlyRate,
+    required this.hoursWorked
+  });
+
+  // polymorphism -> many forms: method overriding
+  @override
+  double calculateAnnualSal(){
+    return 12*hourlyRate*hoursWorked;
+  }
+
+}
 void main(){
-  Faculty faculty = Faculty(
+  Faculty faculty = FullTimeFaculty(
     firstName: "Aryan",
     lastName: "Nakarmi",
     department: "department",
     salary: 10000
     );
-  Faculty faculty2 = Faculty(
+  Faculty faculty2 = FullTimeFaculty(
     firstName: "Amit",
     lastName: "Khayargoli",
     department: "department2",
     salary: 9000
     );
-  Faculty faculty3 = Faculty(
+  Faculty faculty3 = PartTimeFaculty(
     firstName: "Astim",
     lastName: "Suwal",
     department: "department3",
-    salary: 12000
+    hoursWorked: 4,
+    hourlyRate: 500
     );
-  Faculty faculty4 = Faculty(
+  Faculty faculty4 = PartTimeFaculty(
     firstName: "Ishan",
     lastName: "Dhami",
     department: "department4",
-    salary: 10040
+    hoursWorked: 4,
+    hourlyRate: 500
     );
 
 faculty.displayFullName();
